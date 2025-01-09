e0f0b8f4f96c07d1561f72c12d1e3d4e9141f4b0
eric <ericgaona0413@gmail.com>
2025-01-09 19:14:25 +0000

    update the model file

--- mail/models.py
+++ mail/models.py
@@ -7,9 +7,9 @@ class User(AbstractUser):
 
 
 class Email(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
-    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="emails_sent")
-    recipients = models.ManyToManyField("User", related_name="emails_received")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emails")
+    sender = models.ForeignKey(User, on_delete=models.PROTECT, related_name="emails_sent")
+    recipients = models.ManyToManyField(User, related_name="emails_received")
     subject = models.CharField(max_length=255)
     body = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
